f355d6d9697a49296b65a83afd947f342e497a26
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2017-02-10 11:07:24 +0600

    fix nginx repo

--- new-based-software.sh
+++ new-based-software.sh
@@ -4,7 +4,7 @@
 #
 
 # Update and install new software
-yum update -y && yum install epel-release git yum-plugin-priorities net-tools pwgen chrony -y
+yum update -y && yum install epel-release git yum-plugin-priorities net-tools pwgen chrony nano -y
 
 rpm -Uhv http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 # Add nginx repo
@@ -13,7 +13,7 @@ echo "" > /etc/yum.repos.d/nginx.repo && sed -i '1{/^$/d}' /etc/yum.repos.d/ngin
 cat >>/etc/yum.repos.d/nginx.repo <<EOF
 [nginx]
 name=nginx repo
-baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+baseurl=http://nginx.org/packages/centos/\$releasever/\$basearch/
 gpgcheck=0
 enabled=1
 priority=1
@@ -21,7 +21,7 @@ EOF
 
 yum install nginx -y
 systemctl enable nginx
-irewall-cmd --permanent --add-service=http
+firewall-cmd --permanent --add-service=http
 
 # Configure chronyd
 systemctl start chronyd && systemctl enable chronyd
